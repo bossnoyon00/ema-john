d4f8a993cb0ff9385b7f5fd810726bd56fbd2a51
bossnoyon00 <bossnoyon584@gmail.com>
2023-04-05 16:19:36 +0600

    ema-john-update

---
+++ src/loader/cartProductsLoader.js
@@ -0,0 +1,22 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+const cartProductLoader = async () => {
+    const loadedProducts = await fetch('products.json');
+    const products = await loadedProducts.json();
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+        const addedProduct = products.find(pd => pd.id === id)
+        if (addedProduct) {
+            const quantity = storedCart[id]
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+    // return [products, savedCart];
+    // return {products, savedCart};
+    return savedCart;
+}
+
+
+export default cartProductLoader;
