e1bcf115769db4fdda5f3bbf2518cc46726c3c60
bossnoyon00 <bossnoyon584@gmail.com>
2023-04-26 21:43:44 +0600

    first commit

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './Order.css';
 import Cart from '../Cart/Cart';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
     const savedCart = useLoaderData();
@@ -14,7 +14,10 @@ const Orders = () => {
         setCart(remaining);
         removeFromDb(id)
     }
-
+    const handleClearCart = () => {
+        setCart([]);
+        deleteShoppingCart()
+    }
     return (
         <div className='shop-container'>
             <div className='review-container'>
@@ -27,7 +30,15 @@ const Orders = () => {
                 }
             </div>
             <div className='cart-container'>
-                <Cart cart={cart}></Cart>
+                <Cart
+                    cart={cart}
+                    handleClearCart={handleClearCart}
+                >
+                    <Link className='proceed-link' to={'/checkout'}>
+                        <button className='btn-proceed'>Proceed checkout</button>
+                    </Link>
+                </Cart>
+
             </div>
         </div>
     );
